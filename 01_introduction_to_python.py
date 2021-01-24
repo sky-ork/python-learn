cee2ca941e4c097ff76ef8ad56d8012299eb41bd
Evgeniy Kudryavtsev <sky-ork@yandex.ru>
2021-01-24 20:47:20 +0800

    First homework complete

---
+++ 01_introduction_to_python.py
@@ -0,0 +1,28 @@
+name = "Evgeniy"
+print(name)
+
+age = input('Сколько тебе лет? ')
+print(type(age))
+age = int(age)
+print(type(age))
+
+add_age = 10
+sum_age = age + add_age
+
+print(f'через {add_age} лет тебе будет {sum_age}.')
+
+
+# Homeworks...
+
+def solveMeFirst(a, b):
+    # Hint: Type return a+b below
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
+
+print("Hello, World!")
+
+# https://www.hackerrank.com/sky_ork/
